82da216058ff4a4af60e6b2b5edb804a058d0542
mike <mkgalyen@gmail.com>
2022-02-23 12:55:01 -0500

    Fully commented and tested for basic functionality. This code still needs lots of security consideration but this is the state at which it was turned in because I ran out of time.

--- HW3/HW3/main.c
+++ HW3/HW3/main.c
@@ -1,212 +1,252 @@
 #include<stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#define MAX 20
+#include <time.h>
+#define MAX 20 // Limit for buffers
+#define T_SIZE = 100 // Limit for time buffer
 
 
 // Function prototypes
-void fillPassword(size_t , char[]);
 void showResults(char);
-// should have void listed
 void showMenu(void);
 int getUsernameAndCheck(char* un);
-int checkForPassword(char* up);
-void ClearScreen(void);
+int getPasswordAndCheck(char* up);
+void clearScreen(void);
+void printTime(void);
+char * returnTime(void);
+void auditLog(char* message);
 
-// Define a variable to hold a password
-// and the copy
-char password[MAX];
-char cpassword[MAX];
+// Global variables
 char username[MAX];
 char userpassword[MAX];
+char *timeStamp = " ";
+char *secrets_file = "/Users/michaelgalyen/Documents/SDEV_425/HW3/shared_secrets.txt";
+char *audit_log_file = "/Users/michaelgalyen/Documents/SDEV_425/HW3/audit_file.txt";
 FILE *sec_file;
 FILE *audit_file;
 
 
-int main(void)
-{
+// Main routine
+int main(void) {
+    // Print time for user
+    printTime();
+    
     // Welcome the User
     printf("Welcome to the C Array Program!\n");
 
     // Variables
+    char *messageUserName = " Failed username attempt \n"; // Message to use for logging
+    char *messagePassword = " Failed password attempt \n"; // Message to use for logging
+    char *messageLogin = " Successful login \n"; // Message to use for logging
+    char *messageLogout = " User Logged out \n"; // Message to use for logging
     char cont = 'y'; // To continue with loop
-    int cVar = 0; // process variable
-
-    // Call the Copy routine
-//    fillPassword(sizeof(password),password);
-//
-//    // Display variable values
-//    printf("password is %s\n", password);
-//    printf("cVar is %d\n", cVar);
-//
-//    // Copy password
-//    memcpy(cpassword, password,sizeof(password));
-    int checkUserName = 2;
-    int checkUserPassword = 2;
+    int checkUserName = 2; // Int to control login loop
+    int checkUserPassword = 2; // Int to control login loop
     
-    // call to get username ///////////////////////////////////////////////////
-    checkUserName = getUsernameAndCheck(username);
-    //printf("testing %zu \n", sizeof(temp_buf));
-    if (checkUserName == 0) {
-        printf("yes\n");///////////////////////////////////////////////////////////////////////////////
-        printf("This is what we'e looking for %s\n", username);
-        checkUserPassword = checkForPassword(userpassword);
-        if (checkUserPassword == 0) {
-            // Create new lines to obscure username and password
-            ClearScreen();
-            // Display menu and Get Selection
-            while (1) {
-                
-                if (cont == '\n') {
-                    // Get the user selection
-                    cont = getchar();
-                    
-                    // Display the menu response
-                    showResults(cont);
-
-                }
-                if (cont == 'e' || cont == 'E') {
-                    break;
-                }
-                // Diaply the Menu
-                showMenu();
-                
-                // Get the user selection
-                cont = getchar();
-                
-                // Display the menu response
-                showResults(cont);
-            }
+    // While loop to control user login will only exit with a successful login
+    while (checkUserName != 0 || checkUserPassword != 0) {
+        // call to get username
+        checkUserName = getUsernameAndCheck(username);
+        // Check for file not being found
+        if (checkUserName != 0) {
+            // Log file not found
+            auditLog(messageUserName);
         }
-        else {
-            printf("password bad");
+        // call to get password
+        checkUserPassword = getPasswordAndCheck(userpassword);
+        // Check for file not found
+        if (checkUserPassword != 0) {
+            // log file not found
+            auditLog(messagePassword);
         }
     }
-    else {
-        printf("Try again");
+    // Log successful login
+    auditLog(messageLogin);
+    // Call clearScreen() to clear screen by adding a ton \n characters
+    clearScreen();
+    // Display menu and Get Selection
+    
+    // While loop to display sparts seasons menu upon successful login
+    while (1) {
+        // Eat lingering \n from previous interaction
+        if (cont == '\n') {
+            // Get the user selection
+            cont = getchar();
+            // Display the menu response
+            showResults(cont);
+        }
+        // Check if user has entered the quit character
+        if (cont == 'e' || cont == 'E') {
+            break;
+        }
+        // Diaply the Menu
+        showMenu();
+        
+        // Get the user selection
+        cont = getchar();
+        
+        // Display the menu response
+        showResults(cont);
     }
-    char confirm;
+    
+    char confirm; // variable to hold user reponse that quits application
     printf("Confirm your exit by pressing return!\n");
-    confirm = getchar(); //this line was causing yout o have ot hit return twice
+    // Eat previously hanging \n
+    confirm = getchar();
+    // whle loop to wait for user to press enter
     while (getchar() != '\n') {
+        // Get user response
         confirm = getchar();
     }
+    // Log user logout
+    auditLog(messageLogout);
+    // Free memory on program exit
+    free(username);
+    free(userpassword);
+    free(timeStamp);
+    free(secrets_file);
+    free(audit_log_file);
+    free(sec_file);
+    free(audit_file);
+    // Return 0 for successful
     return 0;
 }
 
+// Function takes array as argument and returns a char
+// open password file and get password associated with username
 int getUsernameAndCheck(char* un) {
-    char buf[MAX];
-    char *buff_p = buf;
-    char replace[] = "\n";
-    int result = 2;
-    char val = '0';
-    //fgets(buff_p, MAX, stdin);
+    // Variables
+    char *message = "File not found when retrieving shared_secrets.txt \n"; // Message for audit logging
+    char buf[MAX]; // buffer to hold user input
+    char *buff_p = buf; // Pointr to buffer
+    char replace[] = "\n"; // array used to add \0 to string
+    int result = 2; // Int to return
+    char val = '0'; // Char used to control while loop
+    // While loop to get user input
     while (val == '0') {
+        // Print message
         printf("Please enter username\n");
+        // Get user input
         fgets(buff_p, MAX, stdin);
-        if (strlen(buff_p) > MAX || strlen(buff_p) < 1) {
+        if (strlen(buff_p) > MAX || strlen(buff_p) < 1 || sizeof(buff_p) > 15) {
+            // Print if input invalid
             printf("Try again");
             continue;
         }
+        // input is valid
         else {
+            // Find index of \n
             unsigned long long size = strcspn(buff_p, replace);
+            // replace \n with string terminator
             buff_p[size] = '\0';
-            printf("%s\n", buff_p);
+            // Break out of while loop
             val = '1';
         }
     }
-    sec_file = fopen("/Users/michaelgalyen/Documents/SDEV_425/HW3/shared_secrets.txt", "r");
+    // Open file to read usernames
+    sec_file = fopen(secrets_file, "r");
+    // Check if file didn't exist
     if (sec_file < 0) {
-        audit_file = fopen("/Users/michaelgalyen/Documents/SDEV_425/HW3/audit_file.txt", "a");
-        fputs("File not found when retrieving shared_secrets.txt ", audit_file);
+        // Log file didn't exist
+        auditLog(message);
     }
+    // File existed so parse lines for matching username
     else {
+        // Create buffer to hold lines from file
         char lineBuffer[100];
-        while (fgets(lineBuffer, sizeof(lineBuffer), sec_file)) {
-            puts(lineBuffer);
+        // While loop loops until username is found or end of file character is found
+        while ((fgets(lineBuffer, sizeof(lineBuffer), sec_file)) && (* lineBuffer != EOF || (!feof(stdin) && !ferror(stdin)))) {
+            // Split line on whitespace
             char* original_username = strtok(lineBuffer, " ");
-            //printf("The size of your name is %zu\n", sizeof(original_username));
+            // Check the token created by splitting against the user input
             result = strcmp(buff_p, original_username);
-            printf("%s and %s\n", buff_p, original_username);
-            printf("The result is %d\n", result);
         }
-        
-        
+        // Close file
+        fclose(sec_file);
     }
+    // Once there's a match, copy username out to global array
     if (result == 0) {
         for (int i = 0; i <= sizeof(buff_p); i++) {
             un[i] = buff_p[i];
         }
+        // Return int
         return result;
     }
+    // Return int
     return 1;
 } // End of getUsernameAndCheck()
 
+// Function takes array as argument and returns a char
 // open password file and get password associated with username
-int checkForPassword(char * up) { ///////////////////////DCL20-C. Explicitly specify void when a function accepts no arguments
-    char buf[MAX];
-    char *buff_p = buf;
-    char replace[] = "\n";
-    int result = 2;
-    char val = '0';
-    //fgets(buff_p, MAX, stdin);
+int getPasswordAndCheck(char* up) {
+    // Variables
+    char *message = "File not found when retrieving shared_secrets.txt \n"; // Message for audit log
+    char buf[MAX]; // buffer to hold user input
+    char *buff_p = buf; // Pointer to buffer
+    char replace[] = "\n"; // Char to replace
+    int result = 2; // Int to return
+    char val = '0'; // Int to control while loop
+    // While loop to get user input
     while (val == '0') {
+        // Print message
         printf("Please enter password\n");
+        // Eat previous dangling \n
         fgets(buff_p, MAX, stdin);
-        if (strlen(buff_p) > MAX || strlen(buff_p) < 1) {
+        // Check user input
+        if (strlen(buff_p) > MAX || strlen(buff_p) < 1 || sizeof(buff_p) > 15) {
             printf("Try again");
             continue;
         }
+        // If user input is valid
         else {
+            // Find index of \n
             unsigned long long size = strcspn(buff_p, replace);
+            // replace \n
             buff_p[size] = '\0';
-            printf("%s\n", buff_p);
+            // Break out of loop
             val = '1';
         }
     }
-    sec_file = fopen("/Users/michaelgalyen/Documents/SDEV_425/HW3/shared_secrets.txt", "r");
+    // Open file for credentials
+    sec_file = fopen(secrets_file, "r");
+    // If file doesn't exist
     if (sec_file < 0) {
-        audit_file = fopen("/Users/michaelgalyen/Documents/SDEV_425/HW3/audit_file.txt", "a");
-        fputs("File not found when retrieving shared_secrets.txt ", audit_file);
+        // Log file not found
+        auditLog(message);
     }
+    // Parse through file looking for a matching username and
     else {
+        // buffer to hold lines as they're parsed through
         char lineBuffer[100];
-        while (fgets(lineBuffer, sizeof(lineBuffer), sec_file)) {
-            puts(lineBuffer);
+        // While loop to loop through lines in the file
+        while ((fgets(lineBuffer, sizeof(lineBuffer), sec_file)) && (* lineBuffer != EOF || (!feof(stdin) && !ferror(stdin)))) {
+            // Split line on white space
             char* original_password = strtok(lineBuffer, " ");
-            printf("1 %s\n", original_password);
+            // Move to the next token
             original_password = strtok(NULL, " ");
-            printf("2 %s\n", original_password);
+            // Compare user input with string token
             result = strcmp(buff_p, original_password);
-            printf("%s and %s\n", buff_p, original_password);
-            printf("The result is %d\n", result);
         }
-        
-        
+        // Close file
+        fclose(sec_file);
     }
+    // If there's a match, copy string token out to global array
     if (result == 0) {
         for (int i = 0; i <= sizeof(buff_p); i++) {
             up[i] = buff_p[i];
         }
+        // In case of a matching string, return 0
         return result;
     }
+    // In case of no match, return 1
     return 1;
     
     
     
 }
 
-// Make a String of '1's
-void fillPassword(size_t n, char dest[]) {
-    // Should be n-1
-     for (size_t j = 0; j < n; j++) {
-        dest[j] = '1';
-    }
-    // Add null terminator for string
-    dest[n] = '\0';
-}
-
-/* Display the Results*/
+// Function takes one char as argument and returns nothing
+// Function prints message based on user input
 void showResults(char value) {
     switch (value){
     case 'F':
@@ -233,7 +273,8 @@ void showResults(char value) {
     
 }
 
-/* Display the Menu*/
+// Function takes no arguments and returns nothing
+// Function prints menu
 void showMenu(void) {
     printf("Enter a selection from the following menu.\n");
     printf("B. Baseball season.\n");
@@ -241,9 +282,70 @@ void showMenu(void) {
     printf("S. Soccer season.\n");
     printf("E. Exit the system.\n");
 }
-
-void ClearScreen(void) {
+// Function effectively obscures the user data by printing many new lines
+// Function takes no arguments and returns nothing
+void clearScreen(void) {
     int n;
     for (n = 0; n < 20; n++)
       printf( "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" );
     }
+// Function takes no arguments, prints time to terminal, and return nothing
+void printTime(void) {
+    // Get epoch time
+    time_t t ;
+    // Create pointer to time structure
+    struct tm *tmp;
+    // Create array
+    char MY_TIME[sizeof(t) + 10];
+    // Return time object
+    time( &t );
+    // Fill tm structure
+    tmp = localtime(&t);
+    // Format time
+    strftime(MY_TIME, sizeof(MY_TIME), "%Y-%m-%d %H:%M", tmp);
+    // Thie function prints the time and returns nothing
+    printf("%s\n", MY_TIME );
+}
+// Function takes no arguments and returns the current time as a string
+char * returnTime(void) {
+    // Get epoch time
+    time_t t ;
+    // Pointer to time structure
+    struct tm *tmp;
+    // Create array
+    char MY_TIME[sizeof(t) + 10];
+    // Return time object
+    time( &t );
+    // Fill tm structure
+    tmp = localtime(&t);
+    // Format time
+    strftime(MY_TIME, sizeof(MY_TIME), "%Y-%m-%d %H:%M", tmp);
+    // Allocate memory for time string
+    char *t_str = malloc(sizeof(t) + 1);
+    // Fill time string
+    for (int i = 0; i <= sizeof(MY_TIME); i++) {
+        if (i == sizeof(MY_TIME) + 1) {
+            t_str[i] = '\0';
+        }
+        else {
+            t_str[i] = MY_TIME[i];
+        }
+    }
+    // Return time string
+    return t_str;
+}
+// Function takes one argument and retuns nothing
+void auditLog(char* message) {
+    // Create timestamp
+    for (int i = 0; i < sizeof(returnTime()); i++) {
+        timeStamp = returnTime();
+    }
+    // Open file
+    audit_file = fopen(audit_log_file, "a");
+    // Write timestamp to file
+    fputs(timeStamp, audit_file);
+    // Write argument message
+    fputs(message, audit_file);
+    // Close file
+    fclose(audit_file);
+}
